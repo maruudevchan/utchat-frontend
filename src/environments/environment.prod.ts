8ab127361bf618d4a191e6555c3d105c96cca292
Marie Azonipse <maruudevchan@gmail.com>
2023-04-12 17:42:22 -0500

    netlify try 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrlLocal: 'http://localhost:3000/',
+  apiUrlTotal : 'http://192.168.100.139:3000/',
+
+  apiUrlTotalSocket: 'http://192.168.100.139:3001/',
+  apiUrlLocalSocket: 'http://localhost:3001/',
+
+  //api en la UTRM
+  ApiUrlUtrm: 'http://192.168.49.179:3000/',
+  ApiUrlUtrmSocket: 'http://192.168.49.179:3001/',
+
 };
